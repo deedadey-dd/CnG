3347bcd03d5eea2abf4b2add1c2279fa7c538b06
Dennis on z440 Desktop <deedadey@yahoo.co.uk>
2024-10-21 15:51:13 +0000

    small increment

--- SYNERGY/SYNERGY/settings.py
+++ SYNERGY/SYNERGY/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-2r4j@aprw-@12$wji99*6_x**!$szwl@mtd1m5rvg&a=7!s76a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
